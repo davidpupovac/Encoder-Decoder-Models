8d03c2e97926b4e35403f663ce38efdc56372e82
David Pupovac <david_pupovac@yahoo.com>
2020-04-24 18:31:57 +0200

    encoder-decoder

--- encoder-decoder.py
+++ encoder-decoder.py
@@ -285,6 +285,93 @@ for _ in range(10):
     y_hat = predict_sequence(encoder_model, decoder_model, X1_test, n_steps_out, k_features)
     print('X=%s y=%s, yhat=%s' % (one_hot_decode(X1_test[0]), one_hot_decode(y_test[0]), one_hot_decode(y_hat)))
 
+# =============================================================================    
+# =============================================================================
+
+#              Encoder-decoder models using seq2seq addon
+
+# =============================================================================
+# =============================================================================
+
+# Example includes embedding layer
+
+# -----
+# Simulate toy data 1
+
+def gen_dataset(n_in, n_out, cardinality, n_samples):
+	X1, X2, y = list(), list(), list()
+	for _ in range(n_samples):
+		# generate source sequence
+		source = gen_sequence(n_in, cardinality)
+        
+		# define target sequence:
+        # take first n elements of the source sequence as the target sequence and reverse them
+		target = source[:n_out] # these values will be passed to encoder inputs 
+		target.reverse() # the values are targets
+        
+		# create padded input target sequence
+		target_in = [0] + target[:-1] # include the start of sequence value [i.e. 0] in the first time step
+		# these values will be passed to decoder inputs)
+		
+        # store (create all three inputs)
+		X1.append(source)
+		X2.append(target_in)
+		y.append(target)
+	return array(X1), array(X2), array(y)
+
+k_features = 40 
+n_steps_in = 7 # time steps in 
+n_steps_out = 3 # time steps out
+
+X1, X2, y = gen_dataset(n_steps_in, n_steps_out, k_features, 10000)
+print(X1.shape, X2.shape, y.shape) #
+
+# -----
+import tensorflow_addons as tfa # requires TensorFlow version >= 2.1.0
+import keras
+import tensorflow as tf
+
+tf.random.set_seed(42)
+
+vocab_size = k_features
+embed_size = 10
+ 
+encoder_inputs = tf.keras.layers.Input(shape=[None], dtype=np.int32)
+decoder_inputs = tf.keras.layers.Input(shape=[None], dtype=np.int32)
+sequence_lengths = tf.keras.layers.Input(shape=[], dtype=np.int32)
+
+embeddings = tf.keras.layers.Embedding(vocab_size, embed_size)
+encoder_embeddings = embeddings(encoder_inputs)
+decoder_embeddings = embeddings(decoder_inputs)
+
+encoder = tf.keras.layers.LSTM(512, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_embeddings)
+encoder_state = [state_h, state_c]
+
+sampler = tfa.seq2seq.sampler.TrainingSampler()
+
+decoder_cell = tf.keras.layers.LSTMCell(512)
+output_layer = tf.keras.layers.Dense(vocab_size)
+decoder = tfa.seq2seq.basic_decoder.BasicDecoder(decoder_cell, sampler,
+                                                 output_layer=output_layer)
+
+seq_length_out = np.full([10000], n_steps_out)  # set the lenght of output (it must be vector)
+
+final_outputs, final_state, final_sequence_lengths = decoder(
+    decoder_embeddings, initial_state=encoder_state,
+    sequence_length=seq_length_out) # set the lenght of output
+
+Y_proba = tf.nn.softmax(final_outputs.rnn_output)
+
+model = tf.keras.models.Model(
+    inputs=[encoder_inputs, decoder_inputs, sequence_lengths],
+    outputs=[Y_proba])
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam",metrics=['accuracy'])
+
+seq_length_in = np.full([10000], n_steps_in)
+
+history = model.fit([X1, X2, seq_length_in], y, epochs=5)
 
 # =============================================================================
 # =============================================================================
